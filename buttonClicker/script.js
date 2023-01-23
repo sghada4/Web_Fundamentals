1433331552a68d94cc784486f0c1b96225e5133c
Ghada Sassi <sassighada5@gmail.com>
2023-01-23 19:27:53 +0100

    buttonClicker

---
+++ buttonClicker/script.js
@@ -0,0 +1,11 @@
+function login(id){
+    document.getElementById(id).innerText = "LogOut"
+}
+
+function removeElement(id){
+    document.getElementById(id).remove();
+}
+
+function message(){
+    alert("Ninja was liked");
+}
